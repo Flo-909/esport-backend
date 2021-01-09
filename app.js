e9d85e34e16cf1da390045cae4200ee2dd476fe7
Florian Le Rouzic <lerouzic.florian1@gmail.com>
2021-01-09 10:39:21 +0100

    cors policy back in app.js

--- app.js
+++ app.js
@@ -17,11 +17,11 @@ app.use(cors({
   credentials: true
 }));
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+// app.use(function(req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//   next();
+// });
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
